f591f135a25f351a240fa8beba76d857f638e48e
webermg <matthew.gerald.weber@gmail.com>
2020-11-13 19:39:44 -0800

    added component

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div class="jumbotron">
+        <h1 class="display-4">Google Books Search</h1>
+        <p class="lead">Search for and save books of interest</p>
+        <hr class="my-4" />
+      </div>
+  )
+}
