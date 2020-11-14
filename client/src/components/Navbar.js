2ef366d00d1ea82fb311f032d57280a28bfd5d0a
webermg <matthew.gerald.weber@gmail.com>
2020-11-13 19:40:04 -0800

    rename

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import {Link, useLocation} from 'react-router-dom'
+
+export default function Navbar() {
+  const location = useLocation();
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <span className="navbar-brand mb-0 h1">Google Book Search</span>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <Link to="/search" className={location.pathname === "/" || location.pathname === '/search' ? "nav-link active" : "nav-link"}>
+              Search
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/saved" className={location.pathname === "/saved" ? "nav-link active" : "nav-link"}>
+              Saved
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
