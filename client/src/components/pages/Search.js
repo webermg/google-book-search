d711cd202197f223a439f391e719da6001fba5b2
webermg <matthew.gerald.weber@gmail.com>
2020-11-14 06:19:53 -0800

    added event handlers

--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Header from '../Header'
 import api from '../../util/api'
 import Results from '../Results'
+import SearchForm from '../SearchForm'
 
 export default function Search() {
   const [results, setResults] = React.useState([])
+  const [input,setInput] = React.useState("");
+
   
   React.useEffect(() => {
     api.search("harry+potter").then(res => {
@@ -13,9 +16,24 @@ export default function Search() {
     })
   }, [])
 
+  const searchFormChange = e => {
+    const {name,value} = e.target;
+    setInput({
+      [name]:value
+    })
+    console.log(input)
+  }
+
+  const searchBtnClick = () => {
+    api.search(input.search).then(res=>{
+      setResults(res.data.items);
+    })
+  }
+
   return (
     <div className="container">
       <Header/>
+      <SearchForm onChange={searchFormChange} onClick={searchBtnClick}/>
       {results && <Results books={results}/>}
       {!results && <h1>No books found :(</h1>}
     </div>
