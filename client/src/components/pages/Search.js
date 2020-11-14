d218b5ddc636a2e4711842abc1a95452c33bbb3f
webermg <matthew.gerald.weber@gmail.com>
2020-11-13 19:46:48 -0800

    added results section

--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Header from '../Header'
 
 export default function Search() {
+  const [results, setResults] = React.useState([])
   return (
     <div className="container">
       <Header/>
+      {results && <Results books={results}/>}
     </div>
   )
 }
