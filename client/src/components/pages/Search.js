aa23de96eaa904b702baa5fcef11e1258d5e19fd
webermg <matthew.gerald.weber@gmail.com>
2020-11-13 21:07:18 -0800

    added results

--- client/src/components/pages/Search.js
+++ client/src/components/pages/Search.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Header from '../Header'
+import api from '../../util/api'
+import Results from '../Results'
 
 export default function Search() {
   const [results, setResults] = React.useState([])
+  
+  React.useEffect(() => {
+    api.search("harry+potter").then(res => {
+      console.log(res.data.items)
+      setResults(res.data.items)
+    })
+  }, [])
+
   return (
     <div className="container">
       <Header/>
       {results && <Results books={results}/>}
+      {!results && <h1>No books found :(</h1>}
     </div>
   )
 }
