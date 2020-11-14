25c35b70199a8c9d6ae3aece82264f363149d0c6
webermg <matthew.gerald.weber@gmail.com>
2020-11-14 08:23:51 -0800

    hid api key

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const URL = "https://www.googleapis.com/books/v1/volumes?q=";
-const KEY = process.env.BOOKS_API_KEY || 'AIzaSyC18K0K74Mq0XD7WQ4inBoIpHEzVaEL_Ww'
+const KEY = process.env.BOOKS_API_KEY;
 
 export default {
   search: input => axios.get(`${URL}${input}&key=${KEY}`),
