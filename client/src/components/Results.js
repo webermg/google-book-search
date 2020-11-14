051831ecd46d163531b1ef03a3e4cc4b72affc68
webermg <matthew.gerald.weber@gmail.com>
2020-11-13 21:06:42 -0800

    updated to show api results

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import Book from './Book'
 
 export default function Results(props) {
   return (
     <div className='col-12 d-flex flex-column results'>
-      
+      {props.books.map(book => (<Book 
+        title={book.volumeInfo.title}
+        authors={book.volumeInfo.authors}
+        image={book.volumeInfo.imageLinks.thumbnail}
+        date={book.volumeInfo.publishedDate}
+        publisher={book.volumeInfo.publisher}
+        description={book.volumeInfo.description}
+        infoLink={book.volumeInfo.infoLink}
+        />))}
     </div>
   )
 }
